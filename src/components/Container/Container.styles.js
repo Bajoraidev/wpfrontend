34e48d6877b70665434b59f1a25a3f00d6223ed2
NeringaSun <82957315+NeringaSun@users.noreply.github.com>
2021-12-07 17:29:38 +0200

    added readme file

--- src/components/Container/Container.styles.js
+++ src/components/Container/Container.styles.js
@@ -3,18 +3,4 @@ import styled from 'styled-components';
 export const div = styled.div`
   width: 80%;
   margin: 0 auto;
-  //display: flex;
-  //justify-content: space-between;
-  border: 1px solid pink;
 `;
-
-/*export const div = styled.div`
-  //align-items: right;
-  background-color: #062847;
-  //::afterdisplay: flex;
-  //justify-content: center;
-  min-width: 200rem;
-`;
-export const img = styled.img`
-  max-width: 10%;
-`;*/
